25421cd350783b5f49ad08485c4560af36f9fbfa
takeyuro <takeyuro@icloud.com>
2022-01-03 14:34:24 +0900

    無駄なecho'';を削除

--- Views/edit.php
+++ Views/edit.php
@@ -8,22 +8,22 @@
 		<p class="form_detail">ユーザー情報を変更したい場合は更新ボタンを、削除したい場合は削除ボタンを押してください。</p>
 		<?php
 			include('../Controllers/editController.php');
-			echo '<div class="start_insert">';
-				echo '<form action="update.php" method="post">';
-				echo '<div class="texts_insert">';
-					echo '<label for="name" class="label_insert">名前<br></label>';
-					echo '<input type="text" id="name" name="name" value="' .$data["name"]. '" required class="write_insert"><br>';
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<label for="email" class="label_insert">Email<br></label>';
-					echo '<input type="text" id="email" name="email" value="' .$data["email"]. '" required class="write_insert"><br>';
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<label for="password" class="label_insert">Password<br></label>';
-					echo '<input type="text" id="password" name="password" placeholder="変更する場合は入力してください。" minlength="8" maxlength="15" class="write_insert"><br>';
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<label for="area" class="label_insert">お住まいの地域<br></label>';
+			echo '<div class="start_insert">
+				<form action="update.php" method="post">
+				<div class="texts_insert">
+					<label for="name" class="label_insert">名前<br></label>
+					<input type="text" id="name" name="name" value="' .$data["name"]. '" required class="write_insert"><br>
+				</div>
+				<div class="texts_insert">
+					<label for="email" class="label_insert">Email<br></label>
+					<input type="text" id="email" name="email" value="' .$data["email"]. '" required class="write_insert"><br>
+				</div>
+				<div class="texts_insert">
+					<label for="password" class="label_insert">Password<br></label>
+					<input type="text" id="password" name="password" placeholder="変更する場合は入力してください。" minlength="8" maxlength="15" class="write_insert"><br>
+				</div>
+				<div class="texts_insert">
+					<label for="area" class="label_insert">お住まいの地域<br></label>';
 					$areaList = array("北海道","東北地方","関東地方","中部地方","近畿地方","中国地方","四国地方","九州地方","沖縄県");
 					echo '<select name="area" class="select_insert">';
 					foreach ($areaList as $area) {
@@ -35,42 +35,43 @@
 							echo '<option value="' .$area. '">' .$area. '</option>';
 						}
 					}
-					echo '</select><br>';
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<label for="gender" class="label_insert">性別<br></label>';
+					echo '</select><br>
+				</div>
+				<div class="texts_insert">
+					<label for="gender" class="label_insert">性別<br></label>';
+
 					// ユーザーが男性なら男性に、女性なら女性にチェックマークが入る
 					if ($data["gender"] == 0) {
-						echo '<input type="radio" name="gender" value="0" checked=checked class="label_insert">男';
-						echo '<input type="radio" name="gender" value="1" class="label_insert">女<br>';
+						echo '<input type="radio" name="gender" value="0" checked=checked class="label_insert">男
+						<input type="radio" name="gender" value="1" class="label_insert">女<br>';
 					} else {
-						echo '<input type="radio" name="gender" value="0" class="label_insert">男';
-						echo '<input type="radio" name="gender" value="1" checked=checked class="label_insert">女<br>';
+						echo '<input type="radio" name="gender" value="0" class="label_insert">男
+						<input type="radio" name="gender" value="1" checked=checked class="label_insert">女<br>';
 					}
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<label for="old" class="label_insert">年齢<br></label>';
-					echo '<input type="text" id="old" name="old" value="' .$data["old"]. '" class="old_insert"><br>';
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<label for="memo" class="label_insert">メモ<br></label>';
-					echo '<textarea name="memo" rows="4" cols="20" maxlength="100" class="memo_insert">' .$data["memo"]. '</textarea><br>';
-					echo '<input type="hidden" name="id" value="' .$id. '">';
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<input type="submit" value="更新" class="submit_insert">';
-				echo '</div>';
-				echo '<div class="texts_insert">';
-					echo '<input type="button" onclick="history.back()" value="キャンセル" class="submit_insert">';
-				echo '</div>';
-				echo '</form>';
-				echo '<div class="sakujo_detail">';
-					echo '<form action="delete_check.php" method="post">';
-						echo '<input type="hidden" name="id" value="' .$id. '">';
-						echo '<input type="submit" value="削除" class="sakujo_detail_2">';
-					echo '</form>';
-				echo '</div>';
-			echo '</div>';
+				echo '</div>
+				<div class="texts_insert">
+					<label for="old" class="label_insert">年齢<br></label>
+					<input type="text" id="old" name="old" value="' .$data["old"]. '" class="old_insert"><br>
+				</div>
+				<div class="texts_insert">
+					<label for="memo" class="label_insert">メモ<br></label>
+					<textarea name="memo" rows="4" cols="20" maxlength="100" class="memo_insert">' .$data["memo"]. '</textarea><br>
+					<input type="hidden" name="id" value="' .$id. '">
+				</div>
+				<div class="texts_insert">
+					<input type="submit" value="更新" class="submit_insert">
+				</div>
+				<div class="texts_insert">
+					<input type="button" onclick="history.back()" value="キャンセル" class="submit_insert">
+				</div>
+				</form>
+				<div class="sakujo_detail">
+					<form action="delete_check.php" method="post">
+						<input type="hidden" name="id" value="' .$id. '">
+						<input type="submit" value="削除" class="sakujo_detail_2">
+					</form>
+				</div>
+			</div>';
 		?>
 	</body>
 </html>
